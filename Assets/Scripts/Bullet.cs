ca96ea61556aa9778f65a89797eb39bce329d529
Angus Goucher (Room 203) <angus.goucher@mymail.champlain.edu>
2023-11-15 17:50:55 -0500

    random ball colors!

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,14 +1,19 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Bullet : MonoBehaviour
 {
     private PhysicsCollider _collider;
-
+    private MeshRenderer _renderer;
+    
     private void Awake()
     {
         _collider = GetComponent<PhysicsCollider>();
+        _renderer = GetComponent<MeshRenderer>();
+
+        _renderer.material.color = Color.HSVToRGB(Random.Range(0f, 1f), 0.9f, 0.8f);
     }
 
     private void LateUpdate()
